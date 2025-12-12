39103107b78517414a67664fe78132ddbcf87c13
Xavi <xaviersiguachi3@gmail.com>
2025-12-11 22:16:26 -0500

    Conexion con postgres

--- src/main/java/com/example/pry_portafolio_backend/PryPortafolioBackendApplication.java
+++ src/main/java/com/example/pry_portafolio_backend/PryPortafolioBackendApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class PryPortafolioBackendApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(PryPortafolioBackendApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(PryPortafolioBackendApplication.class, args);
+    }
 
 }
